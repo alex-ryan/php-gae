edfcf25cde17dc170eaa8fe40c42d6125e9bb761
Alex Ryan <ghextramail@gmail.com>
2015-06-24 00:10:57 +0400

    First commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>PHP GAE</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <link type="text/css" href="/assets/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <?php
+		echo 'Hello, World!';
+		echo '<br/>';
+		phpinfo();
+        ?>
+    </body>
+</html>
